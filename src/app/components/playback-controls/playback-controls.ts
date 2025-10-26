95f2206a85a1269c91a9739989844ea1195f2a21
CarlosEL0 <carloslopezochoa56@gmail.com>
2025-10-26 01:28:01 -0600

    proyecto listo

---
+++ src/app/components/playback-controls/playback-controls.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-playback-controls',
+  imports: [],
+  templateUrl: './playback-controls.html',
+  styleUrl: './playback-controls.css'
+})
+export class PlaybackControls {
+
+}
