9976c2c63857016450df2c4f878513d9f2ef21cb
CarlosEL0 <carloslopezochoa56@gmail.com>
2025-11-04 00:03:31 -0600

    lista de reproduccion funcional

--- src/app/components/queue/queue.ts
+++ src/app/components/queue/queue.ts
@@ -1,11 +1,29 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { MusicService } from '../../services/music';
+import { Song } from '../../song.model';
 
 @Component({
   selector: 'app-queue',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule],
   templateUrl: './queue.html',
   styleUrl: './queue.css'
 })
-export class QueueComponent {
+export class QueueComponent implements OnInit{
+  public songs: Song[] = [];
 
+  // Inyecta el servicio (hazlo público para usarlo en el HTML)
+  constructor(public musicService: MusicService) { }
+
+  ngOnInit(): void {
+    // Obtenemos la lista de canciones del servicio
+    this.songs = this.musicService.getSongs();
+  }
+
+  // Función para reproducir una canción desde la lista
+  playSong(song: Song): void {
+    this.musicService.loadSong(song);
+    this.musicService.play();
+  }
 }
